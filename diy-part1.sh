29c1630ed32ddde08ad7328bdad28d22c2526c69
kuole-o <63436555+kuole-o@users.noreply.github.com>
2020-05-06 15:48:59 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -2,7 +2,7 @@
 #=============================================================
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
+# Description: OpenWrt DIY script part 1 (Before Update feeds 更新feeds前运行part1.sh)
 # Lisence: MIT
 # Author: P3TERX
 # Blog: https://p3terx.com
