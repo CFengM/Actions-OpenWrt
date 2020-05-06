f2744da1665c0aca56e56399320a2acd33eced9b
kuole-o <63436555+kuole-o@users.noreply.github.com>
2020-05-06 20:20:28 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,11 +11,5 @@
 # Uncomment a feed source
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-# 添加主题
-cd -
-git clone https://github.com/jerrykuku/luci-theme-argon -b 19.07_stable ./package/feeds/luci/themes
-git clone https://github.com/openwrt-develop/luci-theme-atmaterial.git ./package/feeds/luci/themes
-git clone https://github.com/rosywrt/luci-theme-rosy.git ./package/feeds/luci/themes
-
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
