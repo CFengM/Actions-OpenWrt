9ceefa9e28a721446a1fbfee19b6f451a102d268
kuole-o <63436555+kuole-o@users.noreply.github.com>
2020-05-07 12:38:22 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,10 +9,7 @@
 #=============================================================
 
 # Uncomment a feed source
-# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
-# 添加ssr
-git clone https://github.com/fw876/helloworld openwrt/package/lean
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
